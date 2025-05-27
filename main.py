771142122724b96228286528d9a2fe90c847b48f
= <code@daschaefer.de>
2025-05-27 15:26:52 +0200

    Added model v2

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ import torch
 from ultralytics import YOLO
 
 # Model loading
-model = YOLO('./runs/detect/train/weights/best.pt')  # Can be 'yolov5n' - 'yolov5x6', or 'custom'
+model = YOLO('./runs/detect/train5/weights/best.pt')  # Can be 'yolov5n' - 'yolov5x6', or 'custom'
 
 # Inference on images
-img = "Screenshot1.jpg"  # Can be a file, Path, PIL, OpenCV, numpy, or list of images
+img = "./datasets/labeld1/not_used/images/f222e608-f09a36b4-IMG_20250520_145509.jpg"  # Can be a file, Path, PIL, OpenCV, numpy, or list of images
 
 # Run inference
 results = model(img)
