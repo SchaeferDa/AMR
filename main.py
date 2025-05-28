1026576789741810a9d3614e18b616aab34d0d0e
Anni <anni233441@gmail.com>
2025-05-28 16:33:48 +0200

    new images

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import torch
 from ultralytics import YOLO
 
 # Model loading
-model = YOLO('./runs/detect/train5/weights/best.pt')  # Can be 'yolov5n' - 'yolov5x6', or 'custom'
+model = YOLO('./models/model-v2.pt')  # Can be 'yolov5n' - 'yolov5x6', or 'custom'
 
 # Inference on images
 img = "./datasets/labeld1/not_used/images/f222e608-f09a36b4-IMG_20250520_145509.jpg"  # Can be a file, Path, PIL, OpenCV, numpy, or list of images
