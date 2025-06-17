4383ce031b8e26ae75c10766278b8afecc5b822a
Daniel Schaefer <code@daschaefer.de>
2025-06-17 14:30:04 +0200

    Added inferece as module

--- main.py
+++ main.py
@@ -3,11 +3,10 @@
 import torch, os, time, cv2, numpy
 from ultralytics import YOLO
 from torchvision.transforms import functional as F
+from inference import Inference
 
-torch.backends.cudnn.enabled = False
-
-# Model loading
-model = YOLO('./models/model-v2.pt')  # Can be 'yolov5n' - 'yolov5x6', or 'custom'
+inference = Inference()
+inference.init('./models/model-v4.pt')
 
 # Inference on images
 folder_path = "./datasets/labeld1/not_used/images"  # Can be a file, Path, PIL, OpenCV, numpy, or list of images
@@ -17,8 +16,7 @@ while True:
     try:
         for file_name in files:
             file = os.path.join(folder_path, file_name)
-            results = model(file)
-            img = results[0].plot()
+            img = inference.inference(file)
             cv2.imshow("Live view", img)
             cv2.waitKey(1)
     except KeyboardInterrupt:
